8b3a8656f5bb2d6a179a00cf090464928cf63b4d
dev.roychung96 <dev.roychung96@gmail.com>
2025-03-15 00:27:22 +0800

    chore: added console for survey data

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -9,7 +9,31 @@ import { ThreeDimensionalLight,ThreeDimensionalDarkPanelless } from "survey-core
 export default function SurveyComponent() {
   const model = new Model(json);
   model.applyTheme(ThreeDimensionalLight);
+  model.onComplete.add((survey, options) => {
+    options.showSaveInProgress();
+    console.log(survey.data);
 
+    // const dataObj = { postId: surveyPostId, surveyResult: resultAsStr };
+    // const dataStr = JSON.stringify(dataObj);
+    // const headers = new Headers({ "Content-Type": "application/json; charset=utf-8" });
+    // fetch(surveyServiceUrl + "/post/", {
+    //   method: "POST",
+    //   body: dataStr,
+    //   headers: headers
+    // }).then(response => {
+    //   if (!response.ok) {
+    //     throw new Error("Could not post the survey results");
+    //   }
+    //   // Display the "Success" message (pass a string value to display a custom message)
+    //   options.showSaveSuccess();
+    //   // Alternatively, you can clear all messages:
+    //   // options.clearSaveMessages();
+    // }).catch(error => {
+    //   // Display the "Error" message (pass a string value to display a custom message)
+    //   options.showSaveError();
+    //   console.log(error);
+    // });
+  });
   return (
     <Survey model={model}/>
   );
